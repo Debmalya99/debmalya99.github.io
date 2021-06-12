e9b6dc2d328b1993d51fc54cde72f36a14331c2d
Debmalya99 <debmalyamitra99@gmail.com>
2021-06-12 20:20:15 +0530

    Add files via upload

---
+++ _nuxt/static/1623508761/blog/greatestscifi/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdebmalya99.github.io\u002F_nuxt\u002Fstatic\u002F1623508761",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fgreatestscifi",config:{_app:{basePath:"\u002Fdebmalya99.github.io\u002F",assetsPath:"\u002Fdebmalya99.github.io\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"352c285d"}}}}(null));
