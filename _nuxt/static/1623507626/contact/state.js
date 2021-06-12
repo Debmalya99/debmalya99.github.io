84cc416e685b059b1db40f1fe5b1983b949d92eb
Debmalya99 <debmalyamitra99@gmail.com>
2021-06-12 20:01:52 +0530

    What the hell is happening

---
+++ _nuxt/static/1623507626/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"debmalya99.github.io\u002F_nuxt\u002Fstatic\u002F1623507626",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcontact",config:{_app:{basePath:"debmalya99.github.io\u002F",assetsPath:"debmalya99.github.io\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"804710ff"}}}}(null));
