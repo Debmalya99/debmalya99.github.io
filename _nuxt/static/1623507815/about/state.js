bdc5747e00dce0b8035fb0856eef1e218220fc22
Debmalya99 <debmalyamitra99@gmail.com>
2021-06-12 20:04:59 +0530

    Ah shit here we go again

---
+++ _nuxt/static/1623507815/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdebmalya99.github.io\u002F_nuxt\u002Fstatic\u002F1623507815",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fdebmalya99.github.io\u002F",assetsPath:"\u002Fdebmalya99.github.io\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8f9d90e6"}}}}(null));
